98f0d6de454d18002f88a3902df6cafc7e26af6e
William Garrard <willga@umich.edu>
2025-03-24 14:12:00 -0400

    Lab 15 attempt 1

--- lab15.cpp
+++ lab15.cpp
@@ -1,35 +1,43 @@
 #include<iostream>
 using namespace std;
+    // Function prototypes
+    void getInfo(int& L, int& Ratio);
+    double computeWays(int k, int n);
+    double factorial(int n);
+    /*******************************************************************
+    * getLotteryInfo                                                   *
+    * Gets and validates lottery info from the user and places it in   *
+    * reference parameters referencing variables in the main function. *
+    ********************************************************************/
+void getInfo(int &L, int &Ratio) {
 
-// Function prototypes
-void getInfo(int&, int&);
-double computeWays(int, int);
-double factorial(int);
-
-/*******************************************************************
-* getLotteryInfo                                                   *
-* Gets and validates lottery info from the user and places it in   *
-* reference parameters referencing variables in the main function. *
-********************************************************************/
-
-
-
-/*******************************************************************
-* computeWays                                                      *
-* Computes and returns the number of different possible sets       *
-* of k numbers that can be chosen from a set of n numbers.         *
-* The formula for this is     k!(n- k)!                            *
-*                             --------                             *
-*                                 n!                               *
-* Note that the computation is done in a way that does not require *
-* multiplying two factorials together. This is done to prevent any *
-* intermediate result becoming so large that it causes overflow.   *
-********************************************************************/
+}
 
 
-/*******************************************************************
-* factorial                                                        *
-* This function computes factorials recursively.                   *
-*******************************************************************/
 
-
+    /*******************************************************************
+    * computeWays                                                      *
+    * Computes and returns the number of different possible sets       *
+    * of k numbers that can be chosen from a set of n numbers.         *
+    * The formula for this is     k!(n- k)!                            *
+    *                             --------                             *
+    *                                 n!                               *
+    * Note that the computation is done in a way that does not require *
+    * multiplying two factorials together. This is done to prevent any *
+    * intermediate result becoming so large that it causes overflow.   *
+    ********************************************************************/
+double computeWays(int k, int n)
+{
+    int sol;
+    int frac = n-k;
+    sol = factorial(k) *factorial(frac)/factorial(n);
+    return sol;
+}
+    /*******************************************************************
+    * factorial                                                        *
+    * This function computes factorials recursively.                   *
+    *******************************************************************/
+    double factorial(int n) {
+        if(n == 0) return 1;
+        else return n * factorial(n - 1);
+    }
